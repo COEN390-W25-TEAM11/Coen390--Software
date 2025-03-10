1091ae0d32dabc3cf0219f7e327e35d514d809e9
asleykantonio <asleykantonio@gmail.com>
2025-03-10 11:11:29 -0400

    add toolbar and return button to LightingControlActivity

--- app/src/main/java/com/example/lightingcontrol/LightingControlActivity.java
+++ app/src/main/java/com/example/lightingcontrol/LightingControlActivity.java
@@ -2,15 +2,20 @@ package com.example.lightingcontrol;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class LightingControlActivity extends AppCompatActivity {
 
-    private Switch powerSwitch, sensorSwitch;
+    private SwitchCompat powerSwitch, sensorSwitch;
     private SeekBar brightnessSeekBar;
     private Button settingsBtn, presetBtn, motionLogsBtn;
 
@@ -25,6 +30,11 @@ public class LightingControlActivity extends AppCompatActivity {
         motionLogsBtn = findViewById(R.id.motionLogsButton);
         settingsBtn = findViewById(R.id.settingsButton);
         presetBtn = findViewById(R.id.presetButton);
+
+        // setup toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Lighting Control");
 
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,5 +58,23 @@ public class LightingControlActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    // show the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    // functionalities for toolbar button (return)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.backtomain) {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
