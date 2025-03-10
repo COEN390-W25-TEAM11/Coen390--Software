5f3d92bd729cd4a66c31d8a2c6008eedd0babcdd
asleykantonio <asleykantonio@gmail.com>
2025-03-10 11:32:04 -0400

    add toolbar to PresetActivity

--- app/src/main/java/com/example/lightingcontrol/PresetActivity.java
+++ app/src/main/java/com/example/lightingcontrol/PresetActivity.java
@@ -1,7 +1,14 @@
 package com.example.lightingcontrol;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class PresetActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class PresetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preset);
+
+        // setup toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Presets");
+    }
+
+    // show the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    // functionalities for toolbar button (return)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.backtomain) {
+            Intent lightIntent = new Intent(this, LightingControlActivity.class);
+            startActivity(lightIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
