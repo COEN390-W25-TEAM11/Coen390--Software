3d7f9205b1fbe9fcda6966a5fd1c4de028edc9ab
Louis Choiniere <louischoiniere@outlook.com>
2025-04-07 17:01:20 -0400

    Update light service

--- app/src/main/java/api/LightService.java
+++ app/src/main/java/api/LightService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
@@ -12,26 +13,111 @@ import retrofit2.http.Path;
 
 public interface LightService {
 
-    // Get all the lights in database with the last 50 motion history
+    @GET("/Api")
+    Call<Void> get();
+
+    class GetResponse {
+        public LightResponse[] lights;
+        public SensorResponse[] sensors;
+        public CombinationResponse[] combinations;
+
+
+        public static class LightResponse {
+            public String id;
+            public String name;
+        }
+
+        public static class SensorResponse {
+            public String id;
+            public String name;
+            public MotionResponse[] motion;
+        }
+
+        public static class MotionResponse {
+
+        }
+
+        public static class CombinationResponse {
+            public String id;
+            public String lightId;
+            public String sensorId;
+        }
+    }
+
+    @PATCH("/Api/light/{lightId")
+    Call<Void> updateLight(@Path("lightId") String lightId, @Body UpdateLightModel model);
+
+    class UpdateLightModel {
+        public String name;
+        public boolean overide;
+        public int state;
+
+        public UpdateLightModel(String name, boolean overide, int state) {
+            this.name = name;
+            this.overide = overide;
+            this.state = state;
+        }
+    }
+
+    @DELETE("/Api/light/{lightId")
+    Call<Void> deleteLight(@Path("lightId") String lightId);
+
+    @PATCH("/Api/sensor/{sensorId")
+    Call<Void> updateSensor(@Path("sensorId") String sensorId, @Body UpdateSensorModel model);
+
+    class UpdateSensorModel {
+        public String name;
+        public int sensitivity;
+        public int timeout;
+
+        public UpdateSensorModel(String name, int sensitivity, int timeout) {
+            this.name = name;
+            this.sensitivity = sensitivity;
+            this.timeout = timeout;
+        }
+    }
+
+    @DELETE("/Api/sensor/{sensorId")
+    Call<Void> deleteSensor(@Path("sensorId") String sensorId);
+
+    @POST("Api/assigned")
+    Call<Void> assignLightSensor(@Body AssignLightSensorModel model);
+
+    class AssignLightSensorModel {
+        public String lightId;
+        public String sensorId;
+
+        public AssignLightSensorModel(String lightId, String sensorId) {
+            this.lightId = lightId;
+            this.sensorId = sensorId;
+        }
+    }
+
+    @DELETE("Api/assigned/{id}")
+    Call<Void> deleteAssign(@Path("id") String id);
+
+
+    @Deprecated
     @GET("Light")
     Call<List<LightResponse>> getLights();
 
-    // Get light object by Id
+    @Deprecated
     @GET("Light/{lightId}") // Use the correct route with lightId
     Call<LightResponse> getLightById(@Path("lightId") UUID lightId);
 
-    // Create a light to be stored in the database
+    @Deprecated
     @POST("Light")
     Call<Void> postLight(@Body Light light);
 
-    // Update the settings of a light
+    @Deprecated
     @PATCH("Light/{lightId}")
     Call<Void> patchLight(@Path("lightId") String lightId, @Body LightUpdateDto lightUpdateDto);
 
+    @Deprecated
     @GET("Light/{lightId}/MotionHistory")
     Call<List<MotionHistory>> getMotionByLight(@Path("lightId") UUID lightId);
 
-    // Light object
+    @Deprecated
     class Light {
         private String id, name;
         private boolean Overide;
@@ -70,7 +156,8 @@ public interface LightService {
         }
     }
 
-    // Update light object
+
+    @Deprecated
     class LightUpdateDto {
         public String name;
         public int state;
@@ -84,7 +171,8 @@ public interface LightService {
 
     }
 
-    // Motion history object
+
+    @Deprecated
     class MotionHistory {
         private String id;
         private String dateTime;
@@ -115,7 +203,8 @@ public interface LightService {
         }
     }
 
-    // Response to a light API
+
+    @Deprecated
     class LightResponse {
         private String id;
         private String name;
